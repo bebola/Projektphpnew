76e024cfc07d1e4555ff541d09234791387eed9f
Aleksandra Bębenek <aleksandra.bebenek@host.com>
2021-06-23 21:52:35 +0200

    css

--- app/src/Form/PhotosType.php
+++ app/src/Form/PhotosType.php
@@ -10,6 +10,7 @@ use App\Entity\Photos;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -64,7 +65,7 @@ class PhotosType extends AbstractType
         );
         $builder->add(
             'text',
-            TextType::class,
+            TextareaType::class,
             [
                 'label' => 'label_text',
                 'required' => true,

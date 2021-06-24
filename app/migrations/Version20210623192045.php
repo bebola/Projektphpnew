eeda3a43797ddd846f8ca3ebe892fe7b8a1496fb
Aleksandra Bębenek <aleksandra.bebenek@host.com>
2021-06-24 10:27:15 +0200

    nowe

---
+++ app/migrations/Version20210623192045.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20210623192045 extends AbstractMigration
+{
+    public function getDescription() : string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema) : void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE Photos CHANGE gallery_id gallery_id INT DEFAULT NULL, CHANGE title title VARCHAR(64) NOT NULL');
+        $this->addSql('ALTER TABLE comments CHANGE photos_id photos_id INT DEFAULT NULL');
+    }
+
+    public function down(Schema $schema) : void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE Photos CHANGE gallery_id gallery_id INT NOT NULL, CHANGE title title VARCHAR(255) CHARACTER SET utf8mb4 NOT NULL COLLATE `utf8mb4_unicode_ci`');
+        $this->addSql('ALTER TABLE comments CHANGE photos_id photos_id INT NOT NULL');
+    }
+}
